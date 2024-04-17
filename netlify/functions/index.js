df7f7052e4edbbf54f13338ec47154e3867b90f1
Isaac Maddox <isaacmaddox05@gmail.com>
2024-04-17 13:15:45 -0500

    debug and get working

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -7,17 +7,18 @@ const serverless = require('serverless-http');
 
 const app = express();
 app.set('view engine', 'pug');
-app.set('views', '../../views');
+app.set('views', 'views');
 
 const router = Router();
-app.use(express.json());
-app.use(cookieParser());
+
 const csrfProtection = csrf({ cookie: true });
 
 router.get('/', csrfProtection, (req, res) => {
     res.render('form', { csrfToken: req.csrfToken() });
 })
 
+app.use(express.json());
+app.use(cookieParser());
 app.use('/', router);
 
 export const handler = serverless(app);
