19080acab9dd0d68a4f64220b483a6694ecc7f9e
Isaac Maddox <isaacmaddox05@gmail.com>
2024-04-17 12:47:50 -0500

    use router

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,3 +1,5 @@
+import { Router } from 'express';
+
 const express = require('express');
 const csrf = require('csurf');
 const cookieParser = require('cookie-parser');
@@ -6,14 +8,16 @@ const serverless = require('serverless-http');
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', '../../views');
+
+const router = Router();
 app.use(express.json());
 app.use(cookieParser());
 const csrfProtection = csrf({ cookie: true });
 
-app.get('/', csrfProtection, (req, res) => {
+router.get('/', csrfProtection, (req, res) => {
     res.render('form', { csrfToken: req.csrfToken() });
 })
 
-app.use('/');
+app.use('/', router);
 
 export const handler = serverless(app);
