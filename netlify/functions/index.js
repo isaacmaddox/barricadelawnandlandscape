2bd505e6250502eb53faf67f8c2276c13d3ef132
Isaac Maddox <isaacmaddox05@gmail.com>
2024-04-17 12:43:05 -0500

    init commit

---
+++ netlify/functions/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', '../../views');
+app.use(express.json());
+app.use(cookieParser());
+const csrfProtection = csrf({ cookie: true });
+
+app.get('/', csrfProtection, (req, res) => {
+    res.render('form', { csrfToken: req.csrfToken() });
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
