5abdae71749502acab648faeb93f6f474ab73053
Isaac Maddox <isaacmaddox05@gmail.com>
2024-04-17 14:09:24 -0500

    use different folder name

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -7,7 +7,7 @@ const serverless = require('serverless-http');
 
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', 'page_views');
 
 const router = Router();
 
