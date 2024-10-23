279518167412ddfd3fd41a4471a84df11f1d7278
isaacmaddox <isaacmaddox05@gmail.com>
2024-10-23 10:10:06 -0500

    Try fix remote addr

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -154,7 +154,7 @@ router.use((err, _, res, next) => {
     res.status(403).send({ error: "Bad CSRF token provided." });
 })
 
-app.use(morgan("[:method] :url (:status) - :remote-addr"));
+app.use(morgan(":remote-addr: [:method] :url (:status)"));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/', router);
