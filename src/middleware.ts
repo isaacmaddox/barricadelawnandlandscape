225f5b7df48234c659f4c81db12d9ecc72d282d4
isaacmaddox <isaacmaddox05@gmail.com>
2024-11-01 11:30:16 -0500

    More TS Changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, NextFunction, Request, RequestHandler, Response } from "express";
 
 interface IpMap {
    count: number;
    timestamps: number[];
+   notFoundCount: number;
 }
 
 interface RouteMap {
@@ -12,10 +13,10 @@ interface RouteMap {
 export class Middleware {
    private ips: RouteMap = {};
 
-   removePoweredBy(req: Request, res: Response, next: NextFunction) {
+   removePoweredBy: RequestHandler = (req, res, next) => {
       res.setHeader("X-Powered-By", "");
       next();
-   }
+   };
 
    rateLimit(allowedRequests: number, rollingTime: number) {
       return (req: Request, res: Response, next: NextFunction) => {
@@ -29,7 +30,7 @@ export class Middleware {
 
          const map = this.ips[req.url];
          const now = Date.now();
-         const requestData = map.get(ip) || { count: 0, timestamps: [] };
+         const requestData = map.get(ip) || { count: 0, timestamps: [], notFoundCount: 0 };
 
          requestData.timestamps = requestData.timestamps.filter(
             (timestamp) => now - timestamp < rollingTime * 60 * 1000
@@ -67,6 +68,13 @@ export class Middleware {
          status: "error",
          message: "Something went wrong",
          diagnostics: err,
+      });
+   };
+
+   notFound: RequestHandler = (req, res, next) => {
+      res.status(404).json({
+         status: "error",
+         message: "Not found",
       });
    };
 }
